59332c848fc844646e50eea4a7ed9587392ccf25
eteen <eteensmash@gmail.com>
2024-08-16 20:41:06 -0700

    workign on nav bar extending too tall

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./navBar.css";
 function NavBar() {
   return (
-    <div className="nav-wrapper">
-      <div className="logo-wrapper">
-        <img src="/images/your-logo.png" alt="Your Logo" class="logo" />
+      <div className="nav-wrapper">
+        <div className="logo-wrapper">
+          <img src="/images/your-logo.png" alt="Your Logo" class="logo" />
+        </div>
+        <div className="menu-wrapper">
+          <img src="/images/menu.png" class="menu" alt="menu"></img>
+        </div>
       </div>
-      <div className="menu-wrapper">
-      <img src="/images/menu.png" class="menu" alt="menu"></img>
-      </div>
-    </div>
   );
 }
 
